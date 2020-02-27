024a58757466aa7322895a6d7c6cc936db1f7c4a
JuliDi <20155974+JuliDi@users.noreply.github.com>
2020-02-27 17:21:31 +0100

    Cleanup imports, add comments, end program with panic! on (fatal) errors

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ extern crate clap;
 extern crate serialport;
 extern crate ureq;
 
-use std::io::{self, Write};
-use std::time::Duration;
-
 use clap::{App, AppSettings, Arg};
 use serialport::prelude::*;
-
-use std::{thread, time};
+use std::{thread, time::Duration};
 
 fn main() {
     let matches = App::new("GMC Logger")
@@ -43,10 +39,11 @@ fn main() {
         )
         .get_matches();
 
+    // Get values from command line parameters
     let port_name = matches.value_of("port").unwrap();
     let aid = matches.value_of("AID").unwrap();
     let gid = matches.value_of("GID").unwrap();
-
+    // Configure serial port (for GMC 320 V4 and others)
     let settings = SerialPortSettings {
         baud_rate: 115200,
         data_bits: DataBits::Eight,
@@ -62,47 +59,47 @@ fn main() {
 
             // Clear port by closing pending commands and reading the response
             match port.write(">>".as_bytes()) {
-                Ok(size) => println!("Wrote {} bytes to end all previous commands!", size),
-                Err(e) => eprintln!("{:?}", e),
+                Ok(size) if size == 2 => (),
+                Ok(_) => panic!("Sent wrong number of bytes!"),
+                Err(e) => panic!("{:?}", e),
             }
+
             // Give the device some time to respond
-            thread::sleep(time::Duration::from_millis(200));
+            thread::sleep(Duration::from_millis(200));
 
+            // Clear serial IO buffers
             match port.clear(ClearBuffer::All) {
-                Ok(_) => (),
-                Err(e) => eprintln!("Error clearing buffer! Message: {}", e),
+                Ok(_) => println!("Buffer cleared"),
+                Err(e) => panic!("Error clearing buffer! Message: {}", e),
             }
-            println!("Buffer cleared");
 
+            // Request current CPM value from device
             match port.write(b"<GETCPM>>") {
-                Ok(t) => {
-                    println!("Wrote {} bytes to get the current CPM", t);
-                }
-                Err(e) => eprintln!("{:?}", e),
+                Ok(_) => println!("Getting current CPM..."),
+                Err(e) => panic!("{:?}", e),
             }
 
-            // Force output before continuing the program
+            // Force output before continuing the program (may be unnecessary)
             match port.flush() {
                 _ => (),
             }
 
             // Give the device some time to respond
-            thread::sleep(time::Duration::from_millis(200));
+            thread::sleep(Duration::from_millis(200));
 
+            // Check whether the response has correct length
             match port.bytes_to_read() {
                 Ok(t) if t == 2 => (),
-                _ => panic!("Device's reponse doesn't have the correct length of 2!"),
+                Ok(_) => panic!("Device's reponse doesn't have the correct length of 2!"),
+                Err(e) => panic!("{:?}", e),
             }
-            
+
             let current_cpm: u16; // Counts Per Minute
             let current_usvph: f32; // uSv/h
 
             // Read the answer from the serial port
             match port.read(serial_buf.as_mut_slice()) {
                 Ok(t) => println!("Result: size: {}, content: {:?}", t, &serial_buf[..t]),
-                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
-                    panic!("Reading CPM from port timed out!")
-                }
                 Err(e) => panic!("{:?}", e),
             }
 
@@ -110,17 +107,16 @@ fn main() {
             current_cpm = ((serial_buf[0] as u16) << 8) | serial_buf[1] as u16;
 
             // Convert the CPM to uSv/h
-            current_usvph = current_cpm as f32 * 6.5 / 1000.0;
+            current_usvph = (current_cpm as f32) * 6.5 / 1000.0;
             println!("{} CPM, {} uSv/h", current_cpm, current_usvph);
 
             match publish_result(&aid, &gid, &current_cpm, &current_usvph) {
                 Ok(_) => println!("Data successfully published"),
-                Err(e) => eprintln!("Publishing data failed! Error: '{}'", e),
+                Err(e) => panic!("Publishing data failed! Error: '{}'", e),
             }
         }
         Err(e) => {
-            eprintln!("Failed to open port {}. Error: {}", port_name, e);
-            ::std::process::exit(1);
+            panic!("Failed to open port {}. Error: {}", port_name, e);
         }
     }
 
@@ -137,14 +133,14 @@ fn main() {
             .timeout_read(2_000)
             .call();
         if resp.ok() {
-            // Check whether gmcmap.com returned OK or an error
+            // Check whether gmcmap.com returned OK.ERR0 or an error
             match resp.into_string() {
                 Ok(t) if t.contains("OK.ERR0") => Ok(()),
                 Ok(t) => Err(format!("gmcmap.com returned errorcode {}", t)),
-                Err(e) => panic!("Getting string from response failed with error: {}", e),
+                Err(e) => panic!("Response.into_string() failed with error: {}", e),
             }
         } else {
-            // Handle other errors like missing internet connection
+            // Handle other errors, e.g. missing internet connection
             Err(format!(
                 "http request failed, response stauts: {} - {}",
                 resp.status(),
